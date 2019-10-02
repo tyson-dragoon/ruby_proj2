b245ceaf9d98ee4a75ec84e8a7ebff5ec62ee863
JeelSukhadiya <35500717+JeelSukhadiya@users.noreply.github.com>
2019-10-02 10:40:42 -0400

    as

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,3 @@ Rails.application.routes.draw do
   resources :librarians
   resources :universities# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-root :to => 'universities#index'
