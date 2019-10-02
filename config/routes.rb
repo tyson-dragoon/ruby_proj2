a95e4b8750baa59dd366c92ab6443dc79a204453
JeelSukhadiya <35500717+JeelSukhadiya@users.noreply.github.com>
2019-10-02 10:35:10 -0400

    routing error

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,4 @@ Rails.application.routes.draw do
   resources :librarians
   resources :universities# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+root :to => 'universities#index'
