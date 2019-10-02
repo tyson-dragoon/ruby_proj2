a685f8b5530e2bd34ac650987d90acbfccac2e8e
JeelSukhadiya <35500717+JeelSukhadiya@users.noreply.github.com>
2019-10-02 10:31:43 -0400

    k

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :programs
+  resources :book_histories
+  resources :books
+  resources :libraries
+  resources :students
+  resources :librarians
+  resources :universities# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
